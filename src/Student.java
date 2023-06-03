f54d2b27f20f49638808f0c8fcc94aec582f3fcc
kzielinski <zielinskik@kul.pl>
2023-06-03 14:14:45 +0200

    Dodanie Student.java

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private String name;
+    private String surname;
+    private String subject;
+}
